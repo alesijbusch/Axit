169f83c7e98bbd8eb69c482180edaed8ff974ac5
alesijbusch <alesijbusch@gmail.com>
2022-10-21 00:10:11 +0300

    dev responsive two

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-// const swiper = new Swiper('.swiper', {
-//     loop: true,
-  
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-//   });
+addEventListener('DOMContentLoaded', (event) => {
+    if( document.documentElement.clientWidth <= 576 ){
+    const swiper = new Swiper('.swiper', {
+      loop: true,
+      direction: 'horizontal',
+      centeredSlides: true,
+      spaceBetween: 10,
+      slidesPerView: 1,
+    
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  }
+
+});
+
+
+
+
